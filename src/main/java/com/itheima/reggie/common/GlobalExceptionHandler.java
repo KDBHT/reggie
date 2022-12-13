3c84b272fd221b56901bed6fba3347d08b324200
liwanhui <3178643875@qq.com>
2022-12-13 19:51:51 +0800

    添加保存员工和配置全局异常管理

---
+++ src/main/java/com/itheima/reggie/common/GlobalExceptionHandler.java
@@ -0,0 +1,28 @@
+package com.itheima.reggie.common;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.mvc.Controller;
+
+import java.sql.SQLIntegrityConstraintViolationException;
+
+/*全局异常处理器*/
+@ControllerAdvice(annotations = {RestController.class})
+@ResponseBody
+@Slf4j
+public class GlobalExceptionHandler {
+    @ExceptionHandler(SQLIntegrityConstraintViolationException.class)
+    public R<String> ExceptionHandler(SQLIntegrityConstraintViolationException exception){
+        log.error(exception.getMessage());
+        if (exception.getMessage().contains("Duplicate entry")){
+            String[] strings = exception.getMessage().split(" ");
+            String msg = strings[2]+"已存在";
+            return R.error(msg);
+        }
+        return R.error("未知错误！");
+    }
+
+}
--- src/main/java/com/itheima/reggie/controller/EmployeeController.java
+++ src/main/java/com/itheima/reggie/controller/EmployeeController.java
@@ -7,12 +7,10 @@ import com.itheima.reggie.service.EmployeeService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.DigestUtils;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
+import java.time.LocalDateTime;
 
 @Slf4j
 @RestController
@@ -54,6 +52,20 @@ public class EmployeeController {
     public R<String> logout(HttpServletRequest request){
         request.getSession().removeAttribute("Employee");
         return R.success("退出成功");
+    }
+    @RequestMapping
+    public R<String> save(HttpServletRequest httpServletRequest,@RequestBody Employee employee){
+        log.info("当前获取到的对象为：{}",employee);
+        employee.setCreateTime(LocalDateTime.now());
+        employee.setUpdateTime(LocalDateTime.now());
+        employee.setPassword(DigestUtils.md5DigestAsHex("123456".getBytes()));
+        Long nowloginuserid = (Long)httpServletRequest.getSession().getAttribute("Employee");
+
+        employee.setCreateUser(nowloginuserid);
+        employee.setUpdateUser(nowloginuserid);
+
+        employeeService.save(employee);
+        return R.success("新增员工成功！");
     }
 
 }

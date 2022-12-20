13869b6869e86496c2724b4f36424f0a4bf03f28
liwanhui <3178643875@qq.com>
2022-12-20 20:31:42 +0800

    添加更新、分页、查询单个语句，添加自定义对象映射器映射Long等类型数据并添加进springmvc的映射器列表。

--- src/main/java/com/itheima/reggie/config/WebMvcConfig.java
+++ src/main/java/com/itheima/reggie/config/WebMvcConfig.java
@@ -1,9 +1,16 @@
 package com.itheima.reggie.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.itheima.reggie.common.JacksonObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+import java.util.List;
+
 @Slf4j
 @Configuration
 public class WebMvcConfig extends WebMvcConfigurationSupport {
@@ -15,5 +22,18 @@ public class WebMvcConfig extends WebMvcConfigurationSupport {
         log.info("开始进行静态资源映射.....");
         registry.addResourceHandler("/backend/**").addResourceLocations("classpath:/backend/");
         registry.addResourceHandler("/front/**").addResourceLocations("classpath:/front/");
+    }
+    /*
+    * 扩展mvc框架的消息转换器
+    * */
+
+    @Override
+    protected void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+    //创建消息转换器
+        MappingJackson2HttpMessageConverter messageConverter = new MappingJackson2HttpMessageConverter();
+        //设置消息转换器
+        messageConverter.setObjectMapper(new JacksonObjectMapper());
+        //将上面的消息转换器对象追加到mvc框架的转换器集合中,必须设置第一位参数下标作为加载顺序，否则加载优先级会很低不生效。
+        converters.add(0,messageConverter);
     }
 }
--- src/main/java/com/itheima/reggie/controller/EmployeeController.java
+++ src/main/java/com/itheima/reggie/controller/EmployeeController.java
@@ -1,12 +1,15 @@
 package com.itheima.reggie.controller;
 
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.itheima.reggie.common.R;
 import com.itheima.reggie.entity.Employee;
 import com.itheima.reggie.service.EmployeeService;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.DigestUtils;
+
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
@@ -67,5 +70,34 @@ public class EmployeeController {
         employeeService.save(employee);
         return R.success("新增员工成功！");
     }
-
+    @GetMapping("/page")
+    public R<Page> page(int page,int pageSize,String name){
+        //分页构造器
+        Page pageinfo = new Page(page,pageSize);
+        //条件构造器
+        LambdaQueryWrapper<Employee> lambdaQueryWrapper = new LambdaQueryWrapper<>();
+        //添加过滤条件
+        lambdaQueryWrapper.like(StringUtils.isNotEmpty(name),Employee::getName,name);
+        //添加排序条件
+        lambdaQueryWrapper.orderByDesc(Employee::getUpdateTime);
+        //执行查询
+        employeeService.page(pageinfo,lambdaQueryWrapper);
+        return R.success(pageinfo);
+    }
+    @PutMapping
+    public R<String> update(HttpServletRequest servletRequest,@RequestBody Employee employee){
+        Long empid = (Long) servletRequest.getSession().getAttribute("Employee");
+        employee.setUpdateTime(LocalDateTime.now());
+        employee.setUpdateUser(empid);
+        employeeService.updateById(employee);
+        return R.success("更新用户信息成功");
+    }
+    @GetMapping("/{id}")
+    public R<Employee> getById(@PathVariable Long id){
+        Employee employee = employeeService.getById(id);
+        if (employee!=null){
+            return R.success(employee);
+        }
+        return R.error("没有查询到对应的员工信息！");
+    }
 }
